7d1b7aca27d3e4c2a147ea8a3da4b02d12927484
James Socol <me@jamessocol.com>
2017-02-03 07:54:15 -0500

    Use chai.expect instead of expect.js

--- test/formatters.spec.js
+++ test/formatters.spec.js
@@ -1,13 +1,13 @@
 'use strict';
 
-var expect = require('expect.js');
+var expect = require('chai').expect;
 var formatters = require('../lib/formatters');
 var levels = require('../lib/levels');
 var LogRecord = require('../lib/LogRecord');
 
 describe('format helper', function () {
     it('should replace named strings', function () {
-        expect(formatters.format('hi %(name)s', {name: 'john'})).to.be('hi john');
+        expect(formatters.format('hi %(name)s', {name: 'john'})).to.equal('hi john');
     });
 
     it('should replace several variables', function () {
@@ -16,7 +16,7 @@ describe('format helper', function () {
             foo: 'bar'
         };
         var result = formatters.format('hi %(name)s %(foo)s %(qux)s %(name)s', values);
-        expect(result).to.be('hi john bar undefined john');
+        expect(result).to.equal('hi john bar undefined john');
     });
 });
 
@@ -25,7 +25,7 @@ describe('dateFormat helper', function () {
         var dt = new Date('2012-07-04T12:13:14-0600');
         var format = '%Y-%m-%dT%H:%M:%S %p %a %A %b %B %w %z %%';
         var result = formatters.dateFormat(format, dt);
-        expect(result).to.be('2012-07-04T14:13:14 PM Wed Wednesday Jul July 3 0400 %');
+        expect(result).to.equal('2012-07-04T14:13:14 PM Wed Wednesday Jul July 3 0400 %');
     });
 });
 
@@ -36,7 +36,7 @@ function makeLogRecord (name) {
 describe('Formatter', function () {
     it('should return a formatted message by default', function () {
         var f = new formatters.Formatter();
-        expect(f.format(makeLogRecord())).to.be('msg goes here');
+        expect(f.format(makeLogRecord())).to.equal('msg goes here');
     });
 
     xit('should follow a format', function () {
@@ -44,20 +44,20 @@ describe('Formatter', function () {
         var record = makeLogRecord();
         record.created = new Date('2012-11-10T09:08:07Z');
         record.process = 401;
-        expect(f.format(record)).to.be('2012-11-10T04:08:07 [INFO] foo:12 [401] msg goes here');
+        expect(f.format(record)).to.equal('2012-11-10T04:08:07 [INFO] foo:12 [401] msg goes here');
     });
 
     it('should respect datefmt', function () {
         var f = new formatters.Formatter('%(asctime)s %(message)s', '%a, %b %d, %Y');
         var record = makeLogRecord();
         record.created = new Date('2013-12-11T10:09:08Z');
-        expect(f.format(record)).to.be('Wed, Dec 11, 2013 msg goes here');
+        expect(f.format(record)).to.equal('Wed, Dec 11, 2013 msg goes here');
     });
 
     it('should include logger name', function () {
         var f = new formatters.Formatter('%(name)s');
         var record = makeLogRecord('test');
-        expect(f.format(record)).to.be('test');
+        expect(f.format(record)).to.equal('test');
     });
 
     it('should format in args', function () {
@@ -65,7 +65,7 @@ describe('Formatter', function () {
         var record = makeLogRecord();
         record.args = ['a', 1, 2, {b: 3}];
         record.msg = '%s %s %s %j';
-        expect(f.format(record)).to.be('a 1 2 {"b":3}');
+        expect(f.format(record)).to.equal('a 1 2 {"b":3}');
     });
 
     it('should append stack trace info', function() {
@@ -75,6 +75,6 @@ describe('Formatter', function () {
         exc.stack = 'a stack';
         record.exc = exc;
         record.msg = 'a message';
-        expect(f.format(record)).to.be('a message\na stack');
+        expect(f.format(record)).to.equal('a message\na stack');
     });
 });
--- test/getLogger.spec.js
+++ test/getLogger.spec.js
@@ -1,37 +1,37 @@
 'use strict';
 
-var expect = require('expect.js');
+var expect = require('chai').expect;
 var logging = require('../');
 
 describe('getLogger', function () {
     it('should return a logger', function () {
         var l = logging.getLogger();
-        expect(l).to.be.a(logging.Logger);
+        expect(l).to.be.an.instanceof(logging.Logger);
     });
 
     it('should return the same root logger', function () {
         var left = logging.getLogger();
         var right = logging.getLogger();
-        expect(left).to.be(right);
+        expect(left).to.equal(right);
     });
 
     it('should set parent loggers', function () {
         var root = logging.getLogger();
         var child = logging.getLogger('child');
-        expect(child._parent).to.be(root);
+        expect(child._parent).to.equal(root);
     });
 
     it('should set and fill in ancestry', function () {
         var root = logging.getLogger();
         var grandchild = logging.getLogger('sister.child');
         var sister = logging.getLogger('sister');
-        expect(grandchild._parent).to.be(sister);
-        expect(sister._parent).to.be(root);
+        expect(grandchild._parent).to.equal(sister);
+        expect(sister._parent).to.equal(root);
     });
 
     it('should return the same child loggers', function () {
         var greatleft = logging.getLogger('a.b.c');
         var greatright = logging.getLogger('a.b.c');
-        expect(greatleft).to.be(greatright);
+        expect(greatleft).to.equal(greatright);
     });
 });
