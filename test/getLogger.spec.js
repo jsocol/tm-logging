7c02f14e6fc27ee5484026ca6777e0c3d180b07e
James Socol <me@jamessocol.com>
2017-09-02 18:56:01 -0400

    Test root loggers

--- test/getLogger.spec.js
+++ test/getLogger.spec.js
@@ -2,24 +2,31 @@ const expect = require('chai').expect;
 const logging = require('../');
 
 describe('getLogger', function () {
-    it('should return a logger', function () {
+    it('returns a logger', function () {
         const logger = logging.getLogger();
         expect(logger).to.be.an.instanceof(logging.Logger);
     });
 
-    it('should return the same root logger', function () {
+    it('returns the same root logger', function () {
         const left = logging.getLogger();
         const right = logging.getLogger();
         expect(left).to.equal(right);
     });
 
-    it('should set parent loggers', function () {
+    it('returns root logger for empty names', function () {
+        const noArgRoot = logging.getLogger();
+        const blankStringRoot = logging.getLogger('');
+        const rootRoot = logging.getLogger('root');
+        expect(noArgRoot).to.equal(blankStringRoot).and.to.equal(rootRoot);
+    });
+
+    it('sets parent loggers', function () {
         const root = logging.getLogger();
         const child = logging.getLogger('child');
         expect(child.parent).to.equal(root);
     });
 
-    it('should set and fill in ancestry', function () {
+    it('sets and fill in ancestry', function () {
         const root = logging.getLogger();
         const grandchild = logging.getLogger('sister.child');
         const sister = logging.getLogger('sister');
@@ -27,7 +34,7 @@ describe('getLogger', function () {
         expect(sister.parent).to.equal(root);
     });
 
-    it('should return the same child loggers', function () {
+    it('returns the same child loggers', function () {
         const greatleft = logging.getLogger('a.b.c');
         const greatright = logging.getLogger('a.b.c');
         expect(greatleft).to.equal(greatright);
