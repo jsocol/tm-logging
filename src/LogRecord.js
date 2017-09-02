37d174ad3c33f2756fcb2634a96b0e9c567d8151
James Socol <me@jamessocol.com>
2017-09-02 14:50:03 -0400

    Update LogRecord to a class

--- src/LogRecord.js
+++ src/LogRecord.js
@@ -2,28 +2,36 @@ const util = require('util');
 const formatters = require('./formatters');
 const levels = require('./levels');
 
-function LogRecord(name, level, pathname, func, lineno, msg, args, exc, extra) {
-    this.name = name;
-    this.level = level;
-    this.msg = msg;
-    this.args = args;
-    this.levelName = levels.getLevelName(level);
-    this.pathname = pathname;
-    this.func = func;
-    this.lineno = lineno;
-    this.exc = exc;
-    this.process = process.pid;
-    this.processName = process.title;
-    this.created = new Date;
-    this.extra = extra || {};
+
+class LogRecord {
+    constructor(name, level, pathname, func, lineno, msg, args, exc, extra) {
+        this.name = name;
+        this.level = levels.checkLevel(level);
+        this.levelName = levels.getLevelName(level);
+        this.pathname = pathname;
+        this.func = func;
+        this.lineno = lineno;
+        this.msg = msg;
+        this.args = args;
+        this.exc = exc;
+        this.process = process.pid;
+        this.processName = process.title;
+        this.created = new Date();
+        this.extra = extra || {};
+    }
+
+    get message() {
+        return this.getMessage();
+    }
+
+    getMessage() {
+        if (this.args) {
+            const a = [this.msg].concat(this.args);
+            return util.format.apply(null, a);
+        }
+        return this.msg;
+    }
 }
 
-LogRecord.prototype.getMessage = function() {
-    if (this.args) {
-        const a = [this.msg].concat(this.args);
-        return util.format.apply(null, a);
-    }
-    return this.msg;
-};
 
 module.exports = LogRecord;
